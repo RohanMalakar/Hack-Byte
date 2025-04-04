2329f1f4a3cb5b47e4a221ff68557330116997ef
aGRAWAL <gargbhoomi01@gmail.com>
2025-04-04 20:24:00 +0530

    Navbar Created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import FeminineNavbar from './components/layout/Navbar';
+import Home from './pages/Home';
+import CreatePost from './pages/Create';
+import ReadPost from './pages/Posts';
+import Blogs from './pages/Blogs';
+import Helplines from './pages/Helplines';
+import Partners from './pages/Partners';
+import About from './pages/About';
+import Contact from './pages/Contact';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <div className='h-screen flex flex-col items-center justify-center bg-gray-100'>
-       hello
-    </div>
-  )
-}
+    <Router>
+      <div className="h-screen flex flex-col">
+        {/* Navbar */}
+        <FeminineNavbar />
+        
+        {/* Page Content */}
+        <div className="flex-1 container mx-auto p-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<CreatePost />} />
+            <Route path="/read" element={<ReadPost />} />
+            <Route path="/blogs" element={<Blogs />} />
+            <Route path="/helplines" element={<Helplines />} />
+            <Route path="/partners" element={<Partners />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+};
 
-export default App
+export default App;
