807383dbe3de409fe067462bb5906ea9f4e87cce
ayushi2910 <itsayushiranjan@gmail.com>
2025-04-05 09:22:35 +0530

    added features-post form and contact us page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../components/layout/Navbar'
+
 const Home = () => {
   return (
     <div>
--- frontend/src/pages/Posts.jsx
+++ frontend/src/pages/Posts.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import PostForm from '../components/createposts/PostForm';
 
-const Posts = () => {
+const Post = () => {
   return (
-    <div>
-      <p>Hello</p>
+    <div className="min-h-screen bg-pink-100 py-12 px-10 flex justify-center items-start">
+      <div > 
+        <PostForm />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Post;
